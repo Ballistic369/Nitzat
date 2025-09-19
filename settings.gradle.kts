47fbda2a5014c87cde6a75f324c2212f226e2250
Ballistic369 <85476965+Ballistic369@users.noreply.github.com>
2025-09-19 15:28:45 +0300

    Update settings.gradle.kts
    By default, Gradle only checks Gradle Plugin Portal, not Google’s Maven repo. The Android plugin is hosted on Google’s Maven, so Ineed to declare it in settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
+pluginManagement {
+    repositories {
+        google()          // Required for Android Gradle Plugin
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "NizatSearchApp"
 include(":app")
